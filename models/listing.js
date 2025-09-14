f86264ccc62e3e7ad90c992463af0dfcf71f8d35
Prashant Borde <bordeprashant12@gmail.com>
2025-09-14 14:10:42 +0530

    added a image uploading feature used cloudinary library for it setup cloud

--- models/listing.js
+++ models/listing.js
@@ -11,13 +11,8 @@ const listingSchema = new Schema({
     type: String,
   },
   image: {
-    type: String,
-    default:
-      "https://images.unsplash.com/photo-1756302637887-1c00e98fd0cc?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    set: (v) =>
-      v === ""
-        ? "https://images.unsplash.com/photo-1756302637887-1c00e98fd0cc?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        : v,
+    url: String,
+    filename: String,
   },
   price: {
     type: Number,
--- routes/listing.js
+++ routes/listing.js
@@ -3,26 +3,26 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
-
 const listingController = require("../controllers/listings.js");
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 router
   .route("/")
   //index route
   .get(wrapAsync(listingController.index))
 
-  //create route
-  // .post(
-  //   isLoggedIn,
-  //   validateListing,
-  //   wrapAsync(listingController.createListing)
-  // );
+  // create route
+  .post(
+    isLoggedIn,
+    upload.single("listing[image]"),
+    validateListing,
+    wrapAsync(listingController.createListing)
+  );
 
-  .post((req, res) => {
-    res.send("this", req.body);
-  }),
-  //New route
-  router.get("/new", isLoggedIn, listingController.renderNewform);
+//New route
+router.get("/new", isLoggedIn, listingController.renderNewform);
 
 router
   .route("/:id")
@@ -32,6 +32,7 @@ router
   .put(
     isLoggedIn,
     isOwner,
+    upload.single("listing[image]"),
     validateListing,
     wrapAsync(listingController.updateListing)
   )
